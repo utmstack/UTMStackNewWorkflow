24e07eef3e5c74b2ac9bc26b1e7a85bdc35d7dd4
Yorjander Hernandez Vergara <yorjaKbayero@gmail.com>
2024-11-11 15:11:46 -0500

    delete dependabot

--- .github/scripts/compare_versions.sh
+++ .github/scripts/compare_versions.sh
@@ -50,3 +50,4 @@ echo "script_services=${script_services_output}" >> $GITHUB_OUTPUT
 echo "image_services=${image_services_json}" >> $GITHUB_OUTPUT
 
 ./upload_image.sh "$image_services_output"
+$GITHUB_WORKSPACE/.github/scripts/upload_image.sh
