d70c983700e2edbfef11cf5a473ffe05b60f28af
Yorjander Hernandez Vergara <yorjaKbayero@gmail.com>
2024-11-11 13:16:25 -0500

    logging scripts

--- .github/scripts/compare_versions.sh
+++ .github/scripts/compare_versions.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+echo "Loading file versions.json from: $GITHUB_WORKSPACE/versions.json"
 versions=$(cat "$GITHUB_WORKSPACE/versions.json")
+echo "Versions: $versions"
+
 main_version=$(echo "${versions}" | jq -r '.version')
+echo "Main version: $main_version"
 
 auth_id=$(echo "$CM_PUB_AUTH" | jq -r '.id')
 auth_key=$(echo "$CM_PUB_AUTH" | jq -r '.key')
@@ -12,6 +16,9 @@ image_services=("agent-manager" "backend" "frontend" "user-auditor" "web-pdf")
 api_url="$CM_API/component-versions?master-version=${main_version}"
 api_versions=$(curl -s -H "publisher-key: $auth_key" -H "publisher-id: $auth_id" "${api_url}")
 
+echo "URL: $api_url"
+echo "API versions: $api_versions"
+
 updated_script_services=()
 updated_image_services=()
 
@@ -19,10 +26,15 @@ for service in $(echo "${versions}" | jq -r 'keys_unsorted[] | select(. != "vers
     service_version=$(echo "${versions}" | jq -r --arg s "$service" '.[$s]')
     api_version=$(echo "${api_versions}" | jq -r --arg s "$service" '.[$s]')
 
+    echo "Processing service: $service"
+    echo "Version in file: $service_version, Version in API: $api_version"
+
     if [[ " ${script_services[@]} " =~ " ${service} " ]] && [ "${service_version}" != "${api_version}" ]; then
+        echo "Update script service detected: $service"
         updated_script_services+=("$service")
     fi
     if [[ " ${image_services[@]} " =~ " ${service} " ]] && [ "${service_version}" != "${api_version}" ]; then
+        echo "Update image service detected: $service"
         version="${service_version}"
         updated_image_services+=("{\"name\":\"$service\",\"version\":\"$version\"}")
     fi
@@ -30,6 +42,9 @@ done
 
 script_services_output=$(IFS=,; echo "${updated_script_services[*]}")
 image_services_output="[$(IFS=,; echo "${updated_image_services[*]}")]"
+
+echo "Script Services Updated: $script_services_output"
+echo "Image Services Updated: $image_services_output"
 
 echo "script_services=${script_services_output}" >> $GITHUB_OUTPUT
 echo "image_services=${image_services_json}" >> $GITHUB_OUTPUT
